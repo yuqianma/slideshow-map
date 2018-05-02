3ce88d0c7634daa1884715a6035cb0cc282a55b4
yuqianma <mayq0422@gmail.com>
2018-05-02 18:04:36 +0800

    center path

--- src/Components/Card/Item.js
+++ src/Components/Card/Item.js
@@ -9,7 +9,8 @@ import { svg, svgObject, attr, linearGradient, createRectClip } from '../../Util
 import { timeline } from 'popmotion';
 
 const TRI = {
-  d: 'M20.5,11.3L4.4,2C3.8,1.7,3.1,2.1,3.1,2.7v18.6c0,0.6,0.7,1,1.3,0.7l16.1-9.3C21,12.4,21,11.6,20.5,11.3z',
+  // centered path
+  d: 'M11.12-.73-5-10a.84.84,0,0,0-1.26.73V9.3A.84.84,0,0,0-5,10L11.12.73A.84.84,0,0,0,11.12-.73Z',
   width: 24,
   height: 24,
 };
@@ -44,8 +45,7 @@ export default class Item extends Component {
   }) {
     attr(this.marker)({
       style: `
-      transform-origin: ${TRI.width / 2}px ${TRI.height / 2}px;
-      transform: scale(0.5, 0.5) translate(${-TRI.width*2}px, ${height / 2 - TRI.height / 2}px);
+      transform: translate(${-TRI.width / 2}px, ${height / 2}px) scale(0.5) rotate(0deg);
       `,
       fill: markerColor
     });
